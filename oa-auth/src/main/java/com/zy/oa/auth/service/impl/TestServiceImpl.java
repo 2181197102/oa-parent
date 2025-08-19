7752ad4dd55a11ba9f39d430256b5dbf250a575e
Zeno <2181197102@qq.com>
2025-08-19 10:26:42 +0800

    add code for TestServiceImpl 22

--- oa-auth/src/main/java/com/zy/oa/auth/service/impl/TestServiceImpl.java
+++ oa-auth/src/main/java/com/zy/oa/auth/service/impl/TestServiceImpl.java
@@ -3,6 +3,8 @@ package com.zy.oa.auth.service.impl;
 public class TestServiceImpl {
 
     public void testMethod() {
+        // This is a simple test method to demonstrate functionality.
         System.out.println("This is a test method in TestServiceImpl.");
+        System.out.println("This is a test 。。。。.");
     }
 }
