8e09cad11244039472c602c4b409f6093e4f475c
Zeno <2181197102@qq.com>
2025-08-19 08:26:12 +0800

    add method for TestServiceImpl

--- oa-auth/src/main/java/com/zy/oa/auth/service/impl/TestServiceImpl.java
+++ oa-auth/src/main/java/com/zy/oa/auth/service/impl/TestServiceImpl.java
@@ -1,4 +1,8 @@
 package com.zy.oa.auth.service.impl;
 
 public class TestServiceImpl {
+
+    public void testMethod() {
+        System.out.println("This is a test method in TestServiceImpl.");
+    }
 }
