aca6db268a3d8c821303ac6102d1c51e7eca6716
zgtj-ubuntu-zy <angersbakers@gmail.com>
2025-08-21 14:23:20 +0800

    modify another line for App,java

--- oa-doc/src/main/java/com/zy/oa/App.java
+++ oa-doc/src/main/java/com/zy/oa/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "HellO  im lisi heiheihei... World!" );
-	System,out.println( "this is oa-doc init JAVA file")
+	System.out.println( "this is oa-doc init JAVA file" );
+	System.out.println( "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh" );
     }
 }
