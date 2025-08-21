cc564da6a320ce3792435abc528ddfbb82302f6b
zgtj-ubuntu-zy <angersbakers@gmail.com>
2025-08-21 14:19:49 +0800

    modify im for App,java

--- oa-doc/src/main/java/com/zy/oa/App.java
+++ oa-doc/src/main/java/com/zy/oa/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "HellO  im lisi heiheihei... World!" );
+	System,out.println( "this is oa-doc init JAVA file")
     }
 }
