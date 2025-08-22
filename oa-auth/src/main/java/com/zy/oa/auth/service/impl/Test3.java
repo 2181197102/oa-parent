6e0b0021afff0e4ef5d68069617e5ae5d2ff8ece
Zeno <2181197102@qq.com>
2025-08-22 16:59:53 +0800

    Revert "Revert "Merge pull request #5 from 2181197102/release/v1.2.0""
    This reverts commit 046ec2bb06c3b3b6266ee2efb5e82aab848e709f.

---
+++ oa-auth/src/main/java/com/zy/oa/auth/service/impl/Test3.java
@@ -0,0 +1,9 @@
+package com.zy.oa.auth.service.impl;
+
+public class Test3 {
+    public void testMethod() {
+        // This is a simple test method to demonstrate functionality.
+        System.out.println("This is a test method in Test3.");
+        System.out.println("This is a test 。。。。 in Test3.");
+    }
+}
